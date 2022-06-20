2dbba8a52ab26349b04a4d04b33011db0a87f247
Erik <erichou@rutgers.edu>
2022-06-20 01:48:19 -0400

    added filter for luxon date

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
   // eleventyConfig.addPassthroughCopy("./src/img");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
   return {
     dir: {
       input: "src",
