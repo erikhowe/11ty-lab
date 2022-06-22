4c05d47f5b4ec5694aa0424b0e6cb7fcd6ef3e80
Erik <erichou@rutgers.edu>
2022-06-21 22:16:20 -0400

    add plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,20 @@
 const { DateTime } = require("luxon");
+const CleanCSS = require("clean-css");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  
   eleventyConfig.addWatchTarget("./src/sass/");
+
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
-  // eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/img");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
